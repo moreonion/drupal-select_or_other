2ae09ba08cdd104ddcaad3fb0c5d1efcab544a60
Daniel Braksator <drupal@braksator.com>
2009-10-24 03:59:35 +0000

    Added some CCK support, and js behaviors

--- select_or_other.js
+++ select_or_other.js
@@ -8,7 +8,6 @@ function select_or_other_check_and_show(ele, page_init) {
     speed = "normal";
     ele = $(ele).parents("div.select-or-other")[0];
   }
-
   if ($(ele).find("select.select-or-other-select option:selected[value=select_or_other], input.select-or-other-select:checked[value=select_or_other]").length) {
     $(ele).find("input.select-or-other-other").show(speed);
   }
@@ -17,15 +16,21 @@ function select_or_other_check_and_show(ele, page_init) {
   }
 }
 
-$(document).ready(function () {
-  $("div.select-or-other").each(function () { 
+/**
+ * The Drupal behaviors for the Select (or other) field.
+ */
+Drupal.behaviors.select_or_other = function(context) {
+  $("div.select-or-other:not('.select-or-other-processed')", context)
+    .addClass('select-or-other-processed').each(function () {
     select_or_other_check_and_show(this, true);
   });
+  $("input.select-or-other-select:not('.select-or-other-processed')", context)
+    .addClass('select-or-other-processed').click(function () {
+    select_or_other_check_and_show(this, false);
+  });
+  $("select.select-or-other-select:not('.select-or-other-processed')", context)
+    .addClass('select-or-other-processed').change(function () {
+    select_or_other_check_and_show(this, false);
+  });
+};
 
-  $("input.select-or-other-select").click(function () { 
-    select_or_other_check_and_show(this, false);
-  });
-  $("select.select-or-other-select").change(function () { 
-    select_or_other_check_and_show(this, false);
-  });
-});
