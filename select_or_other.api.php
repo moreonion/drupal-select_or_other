82f4a9f70b40320439e9b09d471c6ffa081a40f6
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2015-11-05 12:00:38 +0100

    Simple code restructuring.

--- select_or_other.api.php
+++ select_or_other.api.php
@@ -15,7 +15,7 @@
  * Options provided using this hook can be overwritten by options provided in
  * the 'Available options' field on the instance settings form.
  */
-function hook_select_or_other_available_options($entity_type, $bundle, $field_name){
+function hook_select_or_other_available_options($entity_type, $bundle, $field_name) {
   if ($entity_type === 'node' && $bundle === 'page' && $field_name === 'field_some_field') {
     return array(
       'keyed|A keyed value',
