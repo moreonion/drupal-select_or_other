a2809554cbff5821765dba85be8a6255f5028746
legolasbo <legolasbo@2480548.no-reply.drupal.org>
2015-12-16 22:30:03 +0100

    Add dummy test to trick testbot into running our unit tests.

---
+++ src/Tests/DummyTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\select_or_other\Tests\DummyTest
+ */
+
+namespace Drupal\select_or_other\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Dummy test to trick testbot into running our unit tests.
+ *
+ * @group select_or_other
+ */
+class DummyTest extends WebTestBase {
+
+  public function testDummy() {
+    $this->assertTrue(TRUE);
+  }
+
+}
