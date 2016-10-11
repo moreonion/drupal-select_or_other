0a03c7fff13cebae0b817bd3d1f77c3416e30a3a
rodrigoaguilera <rodrigoaguilera@408170.no-reply.drupal.org>
2016-10-11 11:14:57 +0200

    Issue #2557833 by rodrigoaguilera: Add new hook_select_or_other_process_alter

--- select_or_other.api.php
+++ select_or_other.api.php
@@ -5,8 +5,9 @@
  * API documentation for Select or other.
  */
 
-
 /**
+ * Implements hook_select_or_other_available_options().
+ *
  * Provide a list of options for a select or other field. Options can be keyed,
  * unkeyed or a combination of both. Keyed values should be entered in a
  * key|value fashion. Unkeyed options will automatically be keyed with a key
@@ -21,5 +22,19 @@ function hook_select_or_other_available_options($entity_type, $bundle, $field_na
       'keyed|A keyed value',
       'unkeyed',
     );
+  }
+}
+
+/**
+ * Implements hook_select_or_other_process_alter().
+ *
+ * Provide a hook to alter the widget depending on the context and form_state.
+ * In this example we remove the attached files and remove the 'other' textfield
+ * depending on the state of the form submitted.
+ */
+function hook_select_or_other_process_alter(&$element, &$form_state, $context) {
+  unset($element['#attached']);
+  if ($form_state['submitted']) {
+    $element['other']['#access'] = FALSE;
   }
 }
